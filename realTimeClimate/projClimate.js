51d60291dedef920600011465035091851385b96
CoolShivas <shivas2710cool00@gmail.com>
2025-04-04 22:14:49 +0530

    Created the interface for the read and write purpose with the help of readline module using promises.

--- realTimeClimate/projClimate.js
+++ realTimeClimate/projClimate.js
@@ -1,5 +1,7 @@
-console.log("Fetch Real-Time Weather Data Using API");
-// // // Here, we are getting the Output on Terminal as :-
-// // // Restarting 'projClimate.js'
-// // // Fetch Real-Time Weather Data Using API
-// // // Completed running 'projClimate.js'
+import readline from "readline/promises";
+
+
+const lineReader = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
