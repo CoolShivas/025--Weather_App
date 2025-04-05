bb2ae1c19a64096144512c930de5e9689e8674d2
CoolShivas <shivas2710cool00@gmail.com>
2025-04-05 17:32:56 +0530

    Use of CLI interface to read and write things as well as close it.

--- realTimeClimate/projClimate.js
+++ realTimeClimate/projClimate.js
@@ -11,3 +11,11 @@ const lineReader = readline.createInterface({
 const API_Key = `3aa94f962094ae7c82dcebf7d91b85e0`;
 const Base_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
+
+
+const city = await lineReader.question(`Enter a city name to get its weather : `);
+lineReader.close();
+
+// // // Here, we are getting the Output on Terminal as :-
+// // // Enter a city name to get its weather : guna
+// // // Completed running 'projClimate.js'
